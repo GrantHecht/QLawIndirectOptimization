1fa801726b2c0c0614f7755e2612af37e7ff2778
Grant Hecht <granthec@buffalo.edu>
2023-03-05 19:32:47 -0500

    Working on fixed point homotopy but don't think it's working quite yet.

--- src/indirectOptimization/minFuelMayerPseudoarclengthNonlinearFunction.jl
+++ src/indirectOptimization/minFuelMayerPseudoarclengthNonlinearFunction.jl
@@ -26,6 +26,39 @@ function minFuelMayerPseudoarclengthNonlinearFunctionWithJacobian!(F, J, x, xi, 
     return nothing
 end
 
+function minFuelMayerFixedPointNonlinearFunction!(F, x, xi, dc, Δs, x1, κ1, mee0, meef_interp, pTuple)
+    # Compute nonlinear function
+    @views minFuelMayerNonlinearFunctionWithInterp!(F[1:6], x[1:6], κ1, mee0, meef_interp, pTuple)
+
+    # Modify function results for fixed point interation
+    @inbounds for i in 1:6
+        F[i] *= F[i]
+        F[i] += (1.0 - x[7])
+        #F[i] *= (x[i] - x1[i])
+    end
+
+    # Final term for step constraint
+    F[7]    = -Δs
+    @inbounds for i in 1:7
+        F[7] += (x[i] - xi[i])*dc[i]
+    end
+end
+
+function minFuelMayerFixedPointNonlinearFunctionWithJacobian!(F,J,x,xi,dc,Δs,x1,κ1,mee0,meef_interp,pTuple,result,mem,config)
+    if J === nothing && F !== nothing
+        minFuelMayerFixedPointNonlinearFunction!(F, x, xi, dc, Δs, x1, κ1, mee0, meef_interp, pTuple)
+    elseif J !== nothing
+        result = ForwardDiff.jacobian!(result,
+                    (F,x) -> minFuelMayerFixedPointNonlinearFunction!(F, x, xi, dc, Δs, x1, κ1, mee0, meef_interp, pTuple),
+                    mem, x, config, Val{false}())
+        J .= DiffResults.jacobian(result)
+        if F !== nothing
+            F .= DiffResults.value(result)
+        end
+    end
+    return nothing
+end
+
 # meef_interp is a container of interpolants for geting final state constraint and time
 function minFuelMayerNonlinearFunctionWithInterp!(F, x, mee0, meef_interp, pTuple)
     # Grab the interpolated state for the current value of κ 
@@ -48,3 +81,27 @@ function minFuelMayerNonlinearFunctionWithInterp!(F, x, mee0, meef_interp, pTupl
     F[5] = xf[5] - val[5]
     F[6] = xf[13]
 end
+
+function minFuelMayerNonlinearFunctionWithInterp!(F, x, κ1, mee0, meef_interp, pTuple)
+    # Grab the interpolated state for the current value of κ 
+    val = getFinalStateAndTime(meef_interp, κ1)
+
+    # Construct full initial state
+    y0  = SVector(mee0[1], mee0[2], mee0[3], mee0[4], mee0[5], mee0[6], pTuple[1].initMass / pTuple[2].MU,
+                    x[1], x[2], x[3], x[4], x[5], x[6], -1.0)
+
+    # Construct problem and solve
+    prob = ODEProblem(ODEFunction{false}(AstroEOMs.meeOptimalControlEOMs), y0, (0.0, val[6]), pTuple)
+    sol  = solve(prob, Vern9(), reltol = 1e-14, abstol = 1e-14, save_everystep = false, save_start = false, initialize_save = false, maxiters = 1e7,
+            sensealg = ForwardDiffSensitivity(convert_tspan=true))
+    xf   = sol[end]
+
+    F[1] = xf[1] - val[1]
+    F[2] = xf[2] - val[2] 
+    F[3] = xf[3] - val[3]
+    F[4] = xf[4] - val[4]
+    F[5] = xf[5] - val[5]
+    F[6] = xf[13]
+end
+
+
--- src/indirectOptimization/pcContinuation.jl
+++ src/indirectOptimization/pcContinuation.jl
@@ -1,4 +1,4 @@
-function computeTangentVector(J)
+function computeTangentVector(J, posDetDirection)
     # Compute nullspace of J
     nulls   = nullspace(J)
 
@@ -22,14 +22,21 @@ function computeTangentVector(J)
 
     # Check determinate condition
     if det(vcat(J, transpose(nullVec))) > 0.0
-        return nullVec
+        if posDetDirection
+            return nullVec
+        else
+            return -nullVec
+        end
     else 
-        return -nullVec
+        if posDetDirection
+            return -nullVec
+        else
+            return nullVec
+        end
     end
 end
 
-
-function pcContinuation(λ0, Δs, mee0, interp, pTuple)
+function pcContinuation(λ0, Δs0, mee0, interp, pTuple; critPointTol = 1e-16)
     # Preallocate for forward diff
     result1 = DiffResults.JacobianResult(zeros(6), rand(7))
     mem1    = zeros(6)
@@ -50,9 +57,12 @@ function pcContinuation(λ0, Δs, mee0, interp, pTuple)
     κs     = zeros(1000)
 
     # Begin loop
-    u[1:6] .= λ0
-    u[7]    = 0.0
-    i       = 0
+    Δs          = Δs0
+    u[1:6]     .= λ0
+    u[7]        = 0.0
+    i           = 0
+    checkDetDir = true
+    posDetDir   = true
     while i < 1000
         # Increment iteration counter
         i += 1
@@ -63,29 +73,155 @@ function pcContinuation(λ0, Δs, mee0, interp, pTuple)
         F      .= DiffResults.value(result1)
         J      .= DiffResults.jacobian(result1)
 
-        # Compute and print determinate of shooting function jacobian
+        # Compute determinate of shooting function jacobian
+        #detJF   = det(view(J, 1:6, 1:6))
+        rcondJ  = 1.0 / cond(view(J, 1:6, 1:6), 1)
+
+        # If determinate is less than critPointTol, perform fixed-point homotopy to find new path
+        println("rcondJ = " * string(rcondJ))
+        println("Δs = " * string(Δs))
+        if rcondJ < critPointTol
+            println("Beginning fixed point homotopy.")
+            newSolutionAccepted = false
+            while !newSolutionAccepted
+                v      .= fpContinuation(u, 1e-2, mee0, interp, pTuple)
+                v[end]  = u[end]
+
+                newSolutionAccepted = true
+                checkDetDir         = true
+                u                  .= v
+
+                # Solve with new u (May want to add corection at some point, but I think the below code needs to be modified)
+                #sol     = nlsolve(only_fj!((F,J,x) -> minFuelMayerPseudoarclengthNonlinearFunctionWithJacobian!(F,J,x,u,zeros(7),0.0,mee0,interp,pTuple,result2,mem2,config2)),
+                #            v; show_trace = false)
+
+                #if sol.f_converged == true
+                #    println("New solution accepted.")
+                #    newSolutionAccepted = true
+                #    #u .= v
+                #    u .= sol.zero
+                #end
+            end
+        else # Continue along current path
+            # Compute tangent vector of J
+            dc      = computeTangentVector(J, posDetDir)
+            
+            # If necessary, make sure we're stepping with increasing κ1
+            if checkDetDir 
+                checkDetDir = false
+                if u[7] + Δs*dc[7] < u[7]
+                    posDetDir = !posDetDir
+                    dc  = computeTangentVector(J, posDetDir)
+                end
+            end
+
+            # Compute initial guess
+            v      .= u .+ Δs*dc
+
+            # Solve problem with NLsolve
+            sol     = nlsolve(only_fj!((F,J,x) -> minFuelMayerPseudoarclengthNonlinearFunctionWithJacobian!(F,J,x,u,dc,Δs,mee0,interp,pTuple,result2,mem2,config2)),
+                        v; show_trace = false)
+
+            # If successful, update solution
+            if sol.f_converged == true
+                Δs *= 1.1
+                κs[i] = sol.zero[end]
+                println("κ1 = " * string(sol.zero[end]))
+                u  .= sol.zero
+            else
+                println("Reducing Δs!")
+                Δs /= 2.0
+            end
+        end
+    end
+end
+
+function fpContinuation(λ0, Δs, mee0, interp, pTuple; exitInitialIterateTol = 1e-7, solutionTol = 1e-12)
+    # Grab componants of λ0
+    y1  = λ0[1:6]
+    κ1  = λ0[7]
+
+    # Iniitialize second continuation term
+    κ2  = 1.0
+
+    # Preallocate for ForwardDiff
+    result  = DiffResults.JacobianResult(zeros(7), rand(7))
+    mem     = zeros(7)
+    config  = ForwardDiff.JacobianConfig((F,x) -> minFuelMayerFixedPointNonlinearFunction!(F,x,zeros(7),zeros(7),Δs,y1,κ1,mee0,interp,pTuple),
+                zeros(7), rand(7))
+
+    # Allocate iteration variables
+    u      = zeros(7)
+    v      = zeros(7)
+    F      = zeros(7)
+    J      = zeros(7, 7)
+
+    # Begin loop
+    u[1:6]     .= y1
+    u[7]        = κ2
+    i           = 0
+    done        = false
+    maxIters    = 10000
+    hasExitedInitialIterate = false
+    while i < maxIters && !done
+        # Increment iteration counter
+        i += 1
+
+        # Get Jacobian of nonlinear function at current step 
+        result  = ForwardDiff.jacobian!(result, (F,x) -> minFuelMayerFixedPointNonlinearFunction!(F,x,u,u,Δs,y1,κ1,mee0,interp,pTuple),
+                                        mem, u, config, Val{false}())
+        F      .= DiffResults.value(result)
+        J      .= DiffResults.jacobian(result)
+
+        # Compute determinate of shooting function jacobian
         detJF   = det(view(J, 1:6, 1:6))
-        println("det = " * string(detJF))
+
+        # If determinate is less than critPointTol, perform fixed-point homotopy to find new path
+        #println("detJ = " * string(detJF))
 
         # Compute tangent vector of J
-        dc      = computeTangentVector(J)
+        dc      = computeTangentVector(view(J, 1:6, :), true)
 
         # Compute initial guess
-        v      .= u .+ Δs*dc
+        if hasExitedInitialIterate == false
+            # Standard initial step
+            v      .= u .+ Δs*dc
+        else
+            # Adjust initial step so we don't step past new solution
+            if (u[7] > 1.0 && u[7] + Δs*dc[7] < 1.0) || (u[7] < 1.0 && u[7] * Δs*dc[7] > 1.0)
+                Δs = (1.0 - u[7]) / dc[7]
+                println("New Δs = " * string(Δs))
+                v .= u .+ Δs*dc
+            else 
+                v .= u .+ Δs*dc
+            end
+        end
 
         # Solve problem with NLsolve
-        sol     = nlsolve(only_fj!((F,J,x) -> minFuelMayerPseudoarclengthNonlinearFunctionWithJacobian!(F,J,x,u,dc,Δs,mee0,interp,pTuple,result2,mem2,config2)),
-                    v; show_trace = false, ftol = 1e-6)
+        sol     = nlsolve(only_fj!((F,J,x) -> minFuelMayerFixedPointNonlinearFunctionWithJacobian!(F,J,x,u,dc,Δs,y1,κ1,mee0,interp,pTuple,result,mem,config)),
+                    v; show_trace = false)
 
         # If successful, update solution
         if sol.f_converged == true
             Δs *= 1.1
-            κs[i] = sol.zero[end]
-            println("κ = " * string(sol.zero[end]))
+            #println("κ2 = " * string(sol.zero[end]))
             u  .= sol.zero
+
+            # If beginning, check if we've left initial iterate
+            if hasExitedInitialIterate == false
+                if abs(u[7] - 1.0) > exitInitialIterateTol
+                    println("Have exited initial iterate.")
+                    hasExitedInitialIterate = true
+                end
+
+            # Otherwise, see if we've returned to 1.0
+            elseif abs(u[7] - 1.0) < solutionTol
+                done = true
+            end
         else
             println("Reducing Δs!")
             Δs /= 2.0
         end
     end
+    return u
 end
